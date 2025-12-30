fdf25722ddb85f7fe73a319c32f081375ebb5b05
Dmitry Pilavskiy <saw.coding@gmail.com>
2025-12-30 20:49:09 +0300

    Complete task 6

--- 06-data-types/finish.js
+++ 06-data-types/finish.js
@@ -13,3 +13,23 @@
  * используя оператор typeof
  */
 
+const string = 'string';
+const number = 77;
+const boolean = true;
+const myNull = null;
+let isUndefined;
+
+const myObject = {
+  bornAge: 1986,
+  city: 'St-Petersburg'
+};
+
+const myArray = [1, 6, 7, 8];
+
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof boolean);
+console.log(typeof myNull);
+console.log(typeof isUndefined);
+console.log(typeof myObject);
+console.log(typeof myArray);
