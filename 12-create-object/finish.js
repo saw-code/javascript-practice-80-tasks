dd92fac55a7afc6d13f8d150ce4b5f06f72a17e4
Dmitry Pilavskiy <saw.coding@gmail.com>
2025-12-31 08:46:06 +0300

    Complete task 12

--- 12-create-object/finish.js
+++ 12-create-object/finish.js
@@ -8,3 +8,13 @@
  * 2. Выведите в консоль строку
  * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
  */
+
+const myObj = {
+  name: 'DimOk',
+  surname: 'Pilavsky',
+  favoriteNumber: 55
+}
+
+const myString = `My name is ${myObj.name} ${myObj.surname} and my favorite number is ${myObj.favoriteNumber}`
+
+console.log(myString)
