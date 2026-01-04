bd441f97e0ae59d3da2caa0fd4be151c5440b3e1
Dmitry Pilavskiy <saw.coding@gmail.com>
2026-01-04 05:21:01 +0300

    Complete task 19

--- 19-push-to-array/finish.js
+++ 19-push-to-array/finish.js
@@ -6,3 +6,11 @@
  *
  * 3. Выведите в консоль длину результирующего массива
  */
+
+const myArray = [true, 77, 'DimOk', false, null, 'Hello']
+
+myArray.push(888, 'Buy')
+
+console.log(myArray)
+
+console.log(myArray.length)
