2c9c456fedc3f92779fb9028a9d606acb46857c0
Dmitry Pilavskiy <saw.coding@gmail.com>
2026-01-09 11:49:49 +0300

    Completed task 25

--- 25-expression-or-statement/finish.js
+++ 25-expression-or-statement/finish.js
@@ -6,4 +6,31 @@
  *  - выражение-инструкция (expression statement)
  */
 
+// expression statement
+15
 
+// statement
+const myObject = {
+  x: 10,
+  y: true,
+}
+
+// expression statement
+myObject.z = 'abc'
+
+// expression statement
+delete myObject.x
+
+// statement
+let newVariable
+
+// expression statement
+newVariable = 30 + 5
+
+// expression statement
+console.log(newVariable)
+
+// statement
+if (newVariable > 10) {
+  console.log(`${newVariable} больше 10`) // <-- newVariable is expression
+}
